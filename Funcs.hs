932c8bb725daf3aa32da27de3256003b1f950ab0
e_ntyo <entyo.contact@gmail.com>
2018-03-16 21:29:36 +0900

    First commit

---
+++ Funcs.hs
@@ -0,0 +1,47 @@
+module Funcs where
+
+firstLetter :: String -> String
+firstLetter "" = "FUCK OFF WHAT THE EMPTY STRING"
+firstLetter a@(x : xs) =
+  "The first letter of "
+    ++ a
+    ++ " is "
+    ++ [x]
+    ++ ". and the rest part of it is "
+    ++ xs
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height | bmi <= skinny   = "underweight"
+                      | bmi <= standard = "normal"
+                      | bmi <= fat      = "fat"
+                      | otherwise       = "whale"
+ where
+  (skinny, standard, fat) = (18.5, 25.0, 30.0)
+  bmi                     = weight / height ^ (2 :: Integer)
+
+initials :: String -> String -> String
+initials firstName lastName = [f] ++ "." ++ [l] ++ "."
+ where
+  (f : _) = firstName
+  (l : _) = lastName
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [ bmi w h | (w, h) <- xs ]
+  where bmi w h = w / h ^ (2 :: Integer)
+
+calcAndFilterBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcAndFilterBmis xs =
+  [ bmi | (w, h) <- xs, let bmi = w / h ^ (2 :: Integer), bmi > 25 ]
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+qs :: (Ord a) => [a] -> [a]
+qs [] = []
+qs (x : xs) =
+  let smallOrEq = [ a | a <- xs, a <= x ]
+      larger    = [ a | a <- xs, a > x ]
+  in  qs smallOrEq ++ [x] ++ qs larger
+
